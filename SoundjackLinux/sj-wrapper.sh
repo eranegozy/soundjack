97717e51fb3537f0cbb99b2ee92e91e425edc4aa
Eran Egozy <eran@egozy.org>
2020-12-14 18:12:02 -0500

    updated to latest version (201112) which includes new cmd line args and no need for per-server IDs

--- SoundjackLinux/sj-wrapper.sh
+++ SoundjackLinux/sj-wrapper.sh
@@ -3,56 +3,23 @@
 export OUTFILE=/root/dev/SoundjackLinux/SJCstart-out
 export DISPLAY=:100
 export PATH=./:/root/dev/SoundjackLinux:$PATH
-name=`uname -n`
 
-case "$name" in
-        soundjack-test-app-2)
-            VAR1="248"
-            VAR2="247"
-            ;;
-
-        soundjack-prod-app-1)
-            VAR1="246"
-            VAR2="246"
-            ;;
-
-        soundjack-prod-app-2)
-            VAR1="245"
-            VAR2="245"
-            ;;
-
-        soundjack-prod-app-3)
-            VAR1="244"
-            VAR2="244"
-            ;;
- 
-        soundjack-prod-app-4)
-            VAR1="243"
-            VAR2="243"
-            ;;
-
-        soundjack-prod-app-5)
-            VAR1="242"
-            VAR2="242"
-            ;;
-
-        *)
-        echo "This is not a known soundjack host, please check hostname or update script"
-        exit 3
-        ;;
-esac
+export SJ_CHANNELS=1
+export SJ_FRAMESIZE=256
+export SJ_JITTERBUF=2
+export SJ_BINDPORT=50050
 
 case "$1" in
     start)
         echo "Starting soundjack"
-        /usr/bin/Xvfb :100 -ac -noreset > /dev/null 2>&1 &
-        /root/dev/SoundjackLinux/SJC200828 SERVER $VAR1 $VAR2 >> $OUTFILE 2>&1 &
+        /usr/bin/Xvfb $DISPLAY -ac -noreset > /dev/null 2>&1 &
+        /root/dev/SoundjackLinux/SJC201112 SERVER $SJ_CHANNELS $SJ_FRAMESIZE $SJ_JITTERBUF $SJ_BINDPORT >> $OUTFILE 2>&1 &
         ;;
 
     stop)
         echo "Stopping soundjack"
 
-        pkill -9 SJC200828
+        pkill -9 SJC201112
         pkill -9 Xvfb
         ;;
 
