203a4302e48e157c5ced93c2c7da3ba39c39d0a8
Eran Egozy <eran@egozy.org>
2021-02-25 19:47:54 -0500

    updated to version 210106

--- SoundjackLinux/sj-wrapper.sh
+++ SoundjackLinux/sj-wrapper.sh
@@ -8,18 +8,18 @@ export SJ_CHANNELS=1
 export SJ_FRAMESIZE=256
 export SJ_JITTERBUF=2
 export SJ_BINDPORT=50050
-
+export SJ_EXE=SJC210106
 case "$1" in
     start)
         echo "Starting soundjack"
         /usr/bin/Xvfb $DISPLAY -ac -noreset > /dev/null 2>&1 &
-        /root/dev/SoundjackLinux/SJC201112 SERVER $SJ_CHANNELS $SJ_FRAMESIZE $SJ_JITTERBUF $SJ_BINDPORT >> $OUTFILE 2>&1 &
+        /root/dev/SoundjackLinux/$SJ_EXE SERVER $SJ_CHANNELS $SJ_FRAMESIZE $SJ_JITTERBUF $SJ_BINDPORT >> $OUTFILE 2>&1 &
         ;;
 
     stop)
         echo "Stopping soundjack"
 
-        pkill -9 SJC201112
+        pkill -9 $SJ_EXE
         pkill -9 Xvfb
         ;;
 
