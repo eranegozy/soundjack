9a68b315819197eabbf989e066912b3c90720ba2
Eran Egozy <eran@egozy.org>
2021-03-01 15:23:37 -0500

    update to latest

--- SoundjackLinux/sj-wrapper.sh
+++ SoundjackLinux/sj-wrapper.sh
@@ -8,7 +8,7 @@ export SJ_CHANNELS=1
 export SJ_FRAMESIZE=256
 export SJ_JITTERBUF=2
 export SJ_BINDPORT=50050
-export SJ_EXE=SJC210106
+export SJ_EXE=SJC210224
 case "$1" in
     start)
         echo "Starting soundjack"
